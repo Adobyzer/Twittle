2564cf0ae1efcabd7148fb963b1c95dcb458469b
Adobyzer <adam.abed02@hotmail.com>
2024-04-02 00:54:44 -0400

    RenderUi add logic Identificator event

---
+++ index.js
@@ -0,0 +1,64 @@
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    alert(tweetInput.value)
+})
+
+document.addEventListener("click",function(e){
+
+    //listen to click on like button with handle function passing uuid as arg
+
+    if(e.target.dataset.like){
+        handleClickLike(e.target.dataset.like)
+    }
+})
+
+function handleClickLike(tweetId){
+    console.log(tweetId)
+}
+
+
+function getFeedHtml(){
+    let feedHtml = ``
+
+
+    /* loop in each  generated tweet and add uuid for source identificator */
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
+}
+
+/* render data from data.js */
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
